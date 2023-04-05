b9a917cce00f5ef5431f0b4f4e38f0c680fee2ea
Collins Chirchir <collinskchirchir@gmail.com>
2023-04-05 12:51:18 +0300

    routes update

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,22 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  
+  resources :trackings, only: [:create, :update, :destroy, :show, :index]
+  resources :packages, only: [:create, :update, :destroy, :show, :index]
+  resources :shipments, only: [:create, :update, :destroy, :show, :index]
+  resources :users, only: [:create, :update, :destroy, :show, :index]
+  post '/signup', to: 'users#create'
+  post "/login", to: "sessions#create"
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  # user auto-login
+  get "/me", to: "users#show"
+
+  # user logout
+  delete "/logout", to: "sessions#destroy"
+
+  # Custom route for fetching tracking info on Homepage
+  post '/search', to: 'searches#create'
+
+  # Routing logic: fallback requests for React Router.
+  # Leave this here to help deploy your app later!
+  get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
 end
